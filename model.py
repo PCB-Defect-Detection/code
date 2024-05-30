4882131f2b0256ca20d749a59c0a689c348881d4
wogus075 <jjh990130@naver.com>
2024-05-30 15:22:06 +0900

    라벨링한 json파일들을 하나의 json파일로 만드는 코드

---
+++ model.py
@@ -0,0 +1,76 @@
+import json
+import os
+from labelme import utils
+import numpy as np
+import glob
+import PIL.Image
+
+def labelme_to_coco(labelme_jsons, save_json_path):
+    coco = {
+        "info": {},
+        "licenses": [],
+        "images": [],
+        "annotations": [],
+        "categories": []
+    }
+
+    categories = {}
+    category_id = 1
+    annotation_id = 1
+
+    for image_id, labelme_json in enumerate(labelme_jsons):
+        with open(labelme_json, 'r') as f:
+            data = json.load(f)
+
+        # Image
+        image_path = os.path.join(os.path.dirname(labelme_json), data['imagePath'])
+        image = PIL.Image.open(image_path)
+        image_info = {
+            "id": image_id,
+            "file_name": data['imagePath'],
+            "width": image.height,
+            "height": image.width
+        }
+        coco['images'].append(image_info)
+
+        for shape in data['shapes']:
+            label = shape['label']
+            if label not in categories:
+                categories[label] = category_id
+                coco['categories'].append({
+                    'id': category_id,
+                    'name': label,
+                    'supercategory': 'none'
+                })
+                category_id += 1
+
+            points = shape['points']
+            polygon = np.array(points).flatten().tolist()
+
+            # Calculate bounding box
+            min_x = min(p[0] for p in points)
+            max_x = max(p[0] for p in points)
+            min_y = min(p[1] for p in points)
+            max_y = max(p[1] for p in points)
+            bbox = [min_x, min_y, max_x - min_x, max_y - min_y]
+            area = bbox[2] * bbox[3]
+
+            annotation = {
+                "id": annotation_id,
+                "image_id": image_id,
+                "category_id": categories[label],
+                "segmentation": [polygon],
+                "area": area,
+                "bbox": bbox,
+                "iscrowd": 0
+            }
+            coco['annotations'].append(annotation)
+            annotation_id += 1
+
+    with open(save_json_path, 'w') as f:
+        json.dump(coco, f)
+
+# 모든 LabelMe JSON 파일을 변환
+labelme_jsons = glob.glob('C:\\Users\\jjh99\\PycharmProjects\\pcb\\pcb_image\\*.json')
+save_json_path = 'C:\\Users\\jjh99\\PycharmProjects\\pcb\\coco_format_1.json'
+labelme_to_coco(labelme_jsons, save_json_path)
